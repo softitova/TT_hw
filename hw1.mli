04253f3309aed9a82c7de42d4d8e93fc83cea3b0
sofatitovass <sof_titova@rambler.ru>
2017-02-21 23:37:21 +0300

    interface was simplified

--- hw1.mli
+++ hw1.mli
@@ -1,11 +1,11 @@
 type peano = Z | S of peano
 
-val print_peano: peano -> string
+(*val print_peano: peano -> string*)
 val peano_of_int: int -> peano
 val int_of_peano: peano -> int
-val div: peano -> peano ->peano
+(*val div: peano -> peano ->peano*)
 val inc: peano -> peano
-val dec: peano -> peano
+(*val dec: peano -> peano*)
 val add: peano -> peano -> peano
 val sub: peano -> peano -> peano
 val mul: peano -> peano -> peano
--- t.ml
+++ t.ml
@@ -3,11 +3,11 @@ open Hw1;;
 print_int (int_of_peano (S (S (Z))));;
 
 
-print_int(int_of_peano (div (peano_of_int(2)) (peano_of_int (1))));;
+(*print_int(int_of_peano (div (peano_of_int(2)) (peano_of_int (1))));;
 print_int(int_of_peano (div (peano_of_int(8)) (peano_of_int (3))));;
 print_int(int_of_peano (div (peano_of_int(2)) (peano_of_int (5))));;
 print_int(int_of_peano (div (peano_of_int(0)) (peano_of_int (1))));;
-
+*)
 
 
 print_string("power testing\n");;
@@ -45,10 +45,10 @@ print_int(int_of_peano (inc (peano_of_int 2)));;
 print_int(int_of_peano (inc (peano_of_int 0)));;
 
 print_string("decrement testing\n");;
-
+(*
 print_int(int_of_peano (dec (peano_of_int 2)));;
 print_int(int_of_peano (dec (peano_of_int 0)));;
-
+*)
 (*
 print_string (Hw1.string_of_lambda (Hw1.lambda_of_string "\\x.\\y.x"));;
 *)
