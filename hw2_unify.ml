4940ee3459f9ac44244a8a700be7ac8db64d3dae
sofatitovass <sof_titova@rambler.ru>
2017-05-09 12:46:21 +0300

    part3 in progress

--- hw2_unify.ml
+++ hw2_unify.ml
@@ -58,7 +58,7 @@ module StringMap = Map.Make (String);;
 
 let rec apply_helper_list y map = 
         let rec impl y z = match y with 
-                |[] -> z
+                |[] -> List.rev z
                 |h::t -> impl t ((apply_helper h map)::z) in
         impl y []
 
@@ -66,14 +66,33 @@ and apply_helper y map  = match y with
         |Var a -> if StringMap.mem a map then StringMap.find a map else (Var a)
         |Fun(a, b) -> Fun(a, apply_helper_list b map);; 
 
-let apply_substitution subst at = 
-        let rec fill_map l m =
-                match l with 
+let apply_substitution x y = 
+        let rec fill_map l m = match l with 
                         [] -> m
-                        |(var, term)::t -> fill_map t (StringMap.add var term m) in
+                        |(ll, rr)::t -> fill_map t (StringMap.add ll rr m) in
 
-        apply_helper at (fill_map subst StringMap.empty);;
+        apply_helper y (fill_map x StringMap.empty);;
+(*----------------------PART |||--------------------------*)
+
+let rec list_check_helper x y res = match y with
+        |[] -> res
+        |(l,r)::t ->list_check_helper x t (res
+                && ((type_to_string(apply_substitution x l) "") =
+                        (type_to_string(apply_substitution x r) "")));;
 
 
-(*let check_solution x y = failwith "Not implemented";;
-let solve_system x = failwith "Not implemented";;*)
+let check_solution x y = list_check_helper x y true;;
+
+
+
+
+(* Test samples *)
+let at0 = Fun("f",[Fun("g",[Var "y"; Var "x"])]);;
+let s1 = [(Var "a", Var "b");(Var "x", Var "b")];;
+print_string(string_of_bool(check_solution ["a", Var"b"; "x", Var"z"] s1));;
+
+
+
+
+
+(*let solve_system x = failwith "Not implemented";;*)
