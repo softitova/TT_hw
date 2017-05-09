a9cb01669f0cd1f57d9004c6809e1dfe06dfb824
sofatitovass <sof_titova@rambler.ru>
2017-05-09 11:26:42 +0300

    part two in progress

--- hw2_unify.ml
+++ hw2_unify.ml
@@ -52,9 +52,28 @@ let print_eq x str = print_system ( [x] )
 let print_equation x = print_eq x "";;
 
 print_string(print_equation(system_to_equation s0));;
-(*
+
+(*----------------------PART || -------------------------*)
+module StringMap = Map.Make (String);;
+
+let rec apply_helper_list y map = 
+        let rec impl y z = match y with 
+                |[] -> z
+                |h::t -> impl t ((apply_helper h map)::z) in
+        impl y []
+
+and apply_helper y map  = match y with
+        |Var a -> if StringMap.mem a map then StringMap.find a map else (Var a)
+        |Fun(a, b) -> Fun(a, apply_helper_list b map);; 
+
+let apply_substitution subst at = 
+        let rec fill_map l m =
+                match l with 
+                        [] -> m
+                        |(var, term)::t -> fill_map t (StringMap.add var term m) in
+
+        apply_helper at (fill_map subst StringMap.empty);;
 
 
-let apply_substitution x y = failwith "Not implemented";;
-let check_solution x y = failwith "Not implemented";;
+(*let check_solution x y = failwith "Not implemented";;
 let solve_system x = failwith "Not implemented";;*)
