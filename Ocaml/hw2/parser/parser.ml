4cc8975e2aca43946603729e807235403d028b84
sofatitovass <sof_titova@rambler.ru>
2017-05-13 13:56:25 +0300

    hw1_peano in progress

--- Ocaml/hw2/parser/parser.ml
+++ Ocaml/hw2/parser/parser.ml
@@ -58,7 +58,7 @@ let rec subst lambda oldVar newVar =
 	|Var(x) -> if (x = oldVar) then Var(newVar) else Var(x)
 	|App(x, y) -> App(subst x oldVar newVar, subst y oldVar newVar) 
 	|Abs(x, y) -> 
-		if (x = oldVar) then Abs(x, y) else Abs(x, subst y oldVar newVar) 
+		if (x = oldVar) then Abs(x, y) else Abs(x, subst y oldVar newVar);;
 
 let is_alpha_equivalent x y = 
 
@@ -90,43 +90,64 @@ let rec free lambda str  =
 
 module My_set = Set.Make (String);;
 
-let free_subst l1 l2 str = 
-	
-	let rec all_free_vars l blocked =
-		match l with
-			Var v ->
-				if My_set.mem v blocked 
-					then My_set.empty
-					else My_set.singleton v
-			| Abs(v, x) ->
-				all_free_vars x (My_set.add v blocked)
-			| App(x, y) ->
-				My_set.union (all_free_vars x blocked) (all_free_vars y blocked) in
+let rec helper l blocked = match l with
+                |Var v ->
+                        if My_set.mem v blocked 
+                                then My_set.empty
+                                else My_set.singleton v
+                |Abs(v, x) ->
+                        helper x (My_set.add v blocked)
+                |App(x, y) ->
+                        My_set.union (helper x blocked) (helper y
+                        blocked);;
 
-	let free_set = all_free_vars l1 My_set.empty in
+
+let free_vars l blocked = My_set.elements (helper l blocked);;
+       
+let free_to_subst l1 l2 str = 
+	
+	let free_set = helper l1 My_set.empty in
 
 	let rec impl free_set blocked_set l2 str = 
 		match l2 with
-		App(x, y)  -> (impl free_set blocked_set x str) &&  (impl free_set blocked_set y str)
+                |App(x, y)  -> (impl free_set blocked_set x str) &&  (impl free_set blocked_set y str)
 		|Abs(x, y) -> if (x = str) then true else (impl free_set (My_set.add x blocked_set) y str)
 		|Var(x)    -> if (x = str) then ((My_set.inter free_set blocked_set) = My_set.empty) else true in
 
 	impl free_set My_set.empty l2 str;;
 
+let rec check_normal xx res = match xx with
+        |Var x -> true && res
+        |App(Abs(l, r), y) -> false
+        |App(x, y) -> (check_normal x res) && (check_normal y res)
+        |Abs(x, y) -> check_normal y res;;
+        
+let is_normal_form x = check_normal x true;;
+
+let rec subst_term lambda oldVar newVar = 
+	match lambda with 
+	|Var(x) -> if (x = oldVar) then newVar else Var(x)
+	|App(x, y) -> App(subst_term x oldVar newVar, subst_term y oldVar newVar) 
+	|Abs(x, y) -> 
+		if (x = oldVar) then Abs(x, y) else Abs(x, subst_term y oldVar newVar);;
 
 
 
+let rec beta_reduction_step xx res = match xx with
+        |Var x -> (Var x, res || false)
+        |App(Abs(l, r), y) -> ((subst_term r l y), true)
+        |App(x, y) -> let l1, r1= beta_reduction_step x res in
+                        if (r1 = false) 
+                        then let l2, r2 = beta_reduction_step y res in
+                        (App(l1, l2), r2)
+                        else (App(l1, y), true)
+        |Abs(x, y) -> let l1 , r1 = beta_reduction_step y res in
+                        (Abs(x, l1), r1);;
+
+let normal_beta_reduction x = let l, r = beta_reduction_step x false in l;;
 
 
-
-
-
-
-
-
-
-
-
-
+print_string( string_of_lambda(normal_beta_reduction (lambda_of_string
+"((\\x.x1)y)w")));;
 
 
