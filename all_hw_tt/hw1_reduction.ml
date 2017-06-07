fce09fe2a6d28e09adbe898280e50ae1a2121514
sofatitovass <sof_titova@rambler.ru>
2017-06-07 15:20:37 +0300

    aded

--- all_hw_tt/hw1_reduction.ml
+++ all_hw_tt/hw1_reduction.ml
@@ -3,10 +3,9 @@ open Hw1
 
 let rec subst lambda oldVar newVar = 
 	match lambda with 
-	|Var(x) -> if (x = oldVar) then Var(newVar) else Var(x)
+	|Var(x)    -> if (x = oldVar) then Var(newVar) else Var(x)
 	|App(x, y) -> App(subst x oldVar newVar, subst y oldVar newVar) 
-	|Abs(x, y) -> 
-		if (x = oldVar) then Abs(x, y) else Abs(x, subst y oldVar newVar);;
+	|Abs(x, y) -> if (x = oldVar) then Abs(x, y) else Abs(x, subst y oldVar newVar);;
 
 let is_alpha_equivalent x y = 
 
@@ -19,8 +18,7 @@ let is_alpha_equivalent x y =
 
 	let rec make x y = match (x, y) with
 		|(Var xx, Var yy) -> (xx = yy)
-		|(App(x1, x2), App(y1, y2)) ->
-			((make x1 y1) && (make  x2  y2))
+		|(App(x1, x2), App(y1, y2)) -> ((make x1 y1) && (make  x2  y2))
 		|(Abs(x1, x2), Abs(y1, y2)) -> 
 			let next_free_character = (name_generation()) in
 			make (subst x2 x1 next_free_character) (subst y2 y1 next_free_character)
@@ -30,39 +28,39 @@ let is_alpha_equivalent x y =
 
 let rec free lambda str  = 
 	match lambda with
-	|Var(x) -> true 
+	|Var(x)    -> true 
 	|App(x, y) -> (free x str) && (free y str)
-	|Abs(x, y) ->
-		if (x = str) then false  else free y str;;
+	|Abs(x, y) -> if (x = str) then false  else free y str;;
 
+(*------------------ FREE VARS ------------------*)
 
 module My_set = Set.Make (String);;
 
 let rec helper l blocked = match l with
-                |Var v ->
-                        if My_set.mem v blocked 
-                                then My_set.empty
-                                else My_set.singleton v
-                |Abs(v, x) ->
-                        helper x (My_set.add v blocked)
-                |App(x, y) ->
-                        My_set.union (helper x blocked) (helper y
-                        blocked);;
-
+                |Var v     -> if My_set.mem v blocked then My_set.empty else My_set.singleton v
+                |Abs(v, x) -> helper x (My_set.add v blocked)
+                |App(x, y) -> My_set.union (helper x blocked) (helper y blocked);;
 
 let free_vars l = My_set.elements (helper l My_set.empty);;
        
+
+(*------------------ FREE TO SUBST ------------------*)
+
 let free_to_subst l1 l2 str = 
 	
 	let free_set = helper l1 My_set.empty in
 
 	let rec impl free_set blocked_set l2 str = 
 		match l2 with
-                |App(x, y)  -> (impl free_set blocked_set x str) &&  (impl free_set blocked_set y str)
+                |App(x, y) -> (impl free_set blocked_set x str) &&  (impl free_set blocked_set y str)
 		|Abs(x, y) -> if (x = str) then true else (impl free_set (My_set.add x blocked_set) y str)
 		|Var(x)    -> if (x = str) then ((My_set.inter free_set blocked_set) = My_set.empty) else true in
 
 	impl free_set My_set.empty l2 str;;
+
+
+
+(*------------------ IS NORMAL FORM ------------------*)
 
 let rec check_normal xx res = match xx with
         |Var x -> true && res
@@ -70,16 +68,17 @@ let rec check_normal xx res = match xx with
         |App(x, y) -> (check_normal x res) && (check_normal y res)
         |Abs(x, y) -> check_normal y res;;
         
+
 let is_normal_form x = check_normal x true;;
+
+
+(*------------------ NORMAL B REDUCTION ------------------*)
 
 let rec subst_term lambda oldVar newVar = 
 	match lambda with 
-	|Var(x) -> if (x = oldVar) then newVar else Var(x)
+	|Var(x)    -> if (x = oldVar) then newVar else Var(x)
 	|App(x, y) -> App(subst_term x oldVar newVar, subst_term y oldVar newVar) 
-	|Abs(x, y) -> 
-		if (x = oldVar) then Abs(x, y) else Abs(x, subst_term y oldVar newVar);;
-
-
+	|Abs(x, y) -> if (x = oldVar) then Abs(x, y) else Abs(x, subst_term y oldVar newVar);;
 
 let rec beta_reduction_step xx res = match xx with
         |Var x -> (Var x, res || false)
@@ -94,6 +93,13 @@ let rec beta_reduction_step xx res = match xx with
 
 let normal_beta_reduction x = let l, r = beta_reduction_step x false in l;;
 
+(*------------------ REDUCE TO NORMAL FORM ------------------*)
+
+let rec reduce_to_normal_form x = if (is_normal_form x) then x else reduce_to_normal_form (normal_beta_reduction x);;
+
+print_string (string_of_lambda (reduce_to_normal_form (lambda_of_string "(\\x.\\y.(x (a d c b)) x y) z")))
+(* print_string(string_of_lambda(reduce_to_normal_form(lambda_of_string"((\\f.(\\x.f (x x)) (\\x.f (x x))) (\\f.\\n.(\\n.n (\\x.\\x.\\y.y) \\x.\\y.x) n (\\f.\\x.f x) ((\\a.\\b.a ((\\a.\\b.\\f.\\x.a f (b f x)) b) \\f.\\x.x) n (f ((\\n.(\\p.p \\x.\\y.x) (n (\\p.\\f.f ((\\p.p \\x.\\y.y) p) ((\\n.\\f.\\x.f (n f x)) ((\\p.p \\x.\\y.y) p))) (\\f.f (\\f.\\x.x) (\\f.\\x.x)))) n))))) \\f.\\x.f (f (f (f (f (f x)))))"))^"\n");; *)
 
 
-let reduce_to_normal_form x = failwith "dfgt";
+
+
